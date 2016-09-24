f8bf34635236ddfece7e6701adee5d08b13e66c1
Marcus Ferrario <imarcusferr@gmail.com>
2016-09-23 19:49:41 -0700

    Revert "Ensure options exists"
    This reverts commit bde38f944e211a451a92e8602a4171560d7e3672.

--- src/emailjs-imap-client-imap.js
+++ src/emailjs-imap-client-imap.js
@@ -271,7 +271,7 @@
                 data.tag += '.p';
                 data.request.tag += '.p';
                 this._clientQueue.splice(index, 0, data);
-            } else if (options && options.priority) {
+            } else if (options.priority) {
               data.tag += '.p';
               data.request.tag += '.p';
               this._clientQueue.splice(0, 0, data);
